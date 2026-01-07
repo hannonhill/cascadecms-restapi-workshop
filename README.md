ef815681c9f548301e76fa99815e1f1d3039127f
Fernando Blanco <nando99@gmail.com>
2026-01-07 14:01:02 -0500

    2026 workshop

---
+++ README.md
@@ -0,0 +1,64 @@
+# CUC26 Cascade CMS Rest API Workshop
+
+We'll be using this repository for our CUC26 [Cascade CMS Rest API](https://www.hannonhill.com/cascadecms/latest/developing-in-cascade/rest-api/index.html) Workshop!
+
+The [original repository](https://github.com/kuklaph/cascade-cms-api) was created by [Philip Kukla](https://github.com/kuklaph). His repository included a JS library that primarily uses NodeJS and Google Scripts but also had a vanilla JS fetch option. Although the library itself was great, the standout piece for me was the Rest API operations documentation! I had been working on my own library on and off for a couple of years now, it focuses on client side JS you can run without a lot of setup but useful enough for a few small client facing scripts. I just never had time to polish and finish it. Until now, kind of! With Philip's permission, I forked his repository, updated a few things here and there and fully replaced the library itself. If you prefer to use NodeJS and/or Google Scripts, I highly suggest you check out his [library](https://github.com/kuklaph/cascade-cms-api)!
+
+## Cascade Documentation
+
+You can find an openAPI representation of Cascade CMS's WSDL operations here:
+
+https://nandoblanco.github.io/cuc24-cascadecms-restapi-workshop/swagger-ui/
+
+All information originates from Cascade CMS's WSDL operations page. In order to access this you will need to use your organization as a subdomain followed by this url: `cascadecms.com/ws/services/AssetOperationService?wsdl`
+
+Example: `yourOrg.cascadecms.com/ws/services/AssetOperationService?wsdl`
+
+## General Usage
+
+To use the library you will need to define your Cascade CMS URL and Cascade CMS [API Key](https://www.hannonhill.com/cascadecms/latest/cascade-basics/account-settings.html#APIKey) (v8.16 and later). The `apiKey` is generated in your Cascade dashboard. The `url` is `yourOrg.cascadecms.com/api/v1/` (this is the current version as of 1/9/2023).
+
+```js
+//Example
+const cmsUrl = "https://xxxx.cascadecms.com/";
+const cmsAPI = "xxxxx-xxx-xxxx-xxx-xxxxxx";
+```
+
+Example Operations
+
+```js
+// read asset
+readAsset({
+	type: "page",
+	id: "page ID",
+})
+	.then(function (result) {
+		console.log(result);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+
+// edit asset
+readAsset({
+	type: "page",
+	id: "page ID",
+})
+	.then(function (result) {
+		result.apiReturn.asset.page.metadata.title = "NEW TITLE";
+		editAsset({
+			asset: result.apiReturn,
+		})
+			.then(function (result) {
+				console.log(result);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+```
+
+More examples in the the example folder.
---
+++ cascade-restapi.js
@@ -0,0 +1,311 @@
+/* Step 1: Add the workshop cms URL*/
+// const cmsUrl = "https://workshops.cascadecms.com/";
+/* Step 2: Create and add your cascade API Key. */
+// Note: Additional details on API Setup found here- https://www.hannonhill.com/cascadecms/latest/cascade-basics/account-settings.html#APIKey*/
+// const cmsAPI = "";
+// Note: If you get the error "The requested asset does not exist" and you're not trying to access an existing asset, please double check your API Key.
+/* Step 3: Save */
+
+const headers = { Authorization: "Bearer " + cmsAPI };
+
+const root = "%252F";
+
+function readAsset(a) {
+	if (a.type == "user") {
+		var url = cmsUrl + "api/v1/read/" + a.type + "/" + a.path;
+	} else if (a.path) {
+		var url = cmsUrl + "api/v1/read/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/read/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	if (a.debug) {
+		console.log("Fetch URL " + url);
+	}
+	return new Promise(function (resolve, reject) {
+		fetch(url, { headers: headers })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ read_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ read_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function editAsset(a) {
+	return new Promise(function (resolve, reject) {
+		fetch(cmsUrl + "api/v1/edit", { method: "POST", headers: headers, body: JSON.stringify({ asset: a.asset.asset }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ edit_status: "Success", sent: a, apiReturn: data });
+				} else {
+					reject({ edit_status: "Error", error: data.message, sent: a, apiReturn: data });
+				}
+			});
+	});
+}
+
+function copyAsset(a) {
+	if (a.path) {
+		var url = cmsUrl + "api/v1/copy/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/copy/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	return new Promise(function (resolve, reject) {
+		fetch(url, { method: "POST", headers: headers, body: JSON.stringify({ copyParameters: a.copyParameters }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ copy_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ copy_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function moveAsset(a) {
+	if (a.path) {
+		var url = cmsUrl + "api/v1/move/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/move/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	return new Promise(function (resolve, reject) {
+		fetch(url, { method: "POST", headers: headers, body: JSON.stringify({ moveParameters: a.moveParameters }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ move_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ move_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function deleteAsset(a) {
+	if (a.path) {
+		var url = cmsUrl + "api/v1/delete/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/delete/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	return new Promise(function (resolve, reject) {
+		fetch(url, { method: "POST", headers: headers })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ delete_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ delete_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function createAsset(a) {
+	return new Promise(function (resolve, reject) {
+		fetch(cmsUrl + "api/v1/create", { method: "POST", headers: headers, body: JSON.stringify({ asset: a.asset }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ create_status: "Success", sent: a, apiReturn: data });
+				} else {
+					reject({ create_status: "Error", error: data.message, sent: a, apiReturn: data });
+				}
+			});
+	});
+}
+
+function listSubscribers(a) {
+	if (a.path) {
+		var url = cmsUrl + "api/v1/listSubscribers/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/listSubscribers/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	if (a.debug) {
+		console.log("Fetch URL " + url);
+	}
+	return new Promise(function (resolve, reject) {
+		fetch(url, { headers: headers })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ listSubscribers_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ listSubscribers_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function listSites(a) {
+	return new Promise(function (resolve, reject) {
+		fetch(cmsUrl + "api/v1/listSites", { headers: headers })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ listSites_status: "Success", sent: a, apiReturn: data });
+				} else {
+					reject({ listSites_status: "Error", error: data.message, sent: a, apiReturn: data });
+				}
+			});
+	});
+}
+
+function copySite(a) {
+	return new Promise(function (resolve, reject) {
+		fetch(cmsUrl + "api/v1/siteCopy", { method: "POST", headers: headers, body: JSON.stringify(a) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ edit_status: "Success", sent: a, apiReturn: data });
+				} else {
+					reject({ edit_status: "Error", error: data.message, sent: a, apiReturn: data });
+				}
+			});
+	});
+}
+
+function search(a) {
+	return new Promise(function (resolve, reject) {
+		fetch(cmsUrl + "api/v1/search", { method: "POST", headers: headers, body: JSON.stringify({ searchInformation: a.searchInformation }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ search_status: "Success", sent: a, apiReturn: data });
+				} else {
+					reject({ search_status: "Error", error: data.message, sent: a, apiReturn: data });
+				}
+			});
+	});
+}
+
+function publishAsset(a) {
+	if (a.path) {
+		var url = cmsUrl + "api/v1/publish/" + a.type + "/" + a.siteName + "/" + a.path;
+	} else if (a.id) {
+		var url = cmsUrl + "api/v1/publish/" + a.type + "/" + a.id;
+	}
+	url = url.replace(/\\\\/g, "\\");
+	if (a.debug) {
+		console.log("Fetch URL " + url);
+	}
+	return new Promise(function (resolve, reject) {
+		fetch(url, { method: "POST", headers: headers, body: JSON.stringify({ publishInformation: a.publishInformation }) })
+			.then((r) => r.json())
+			.then((data) => {
+				if (data.success) {
+					resolve({ read_status: "Success", sent: a, apiReturn: data, url: url });
+				} else {
+					reject({ read_status: "Error", error: data.message, sent: a, apiReturn: data, url: url });
+				}
+			});
+	});
+}
+
+function dateToDateTime(d) {
+	var d = new Date(d);
+	return new Date(d.getTime() - d.getTimezoneOffset() * 60000).toISOString();
+}
+
+function v(text) {
+	if (text === null || text === undefined || text === "") {
+		return "";
+	} else {
+		return text;
+	}
+}
+
+function w(text) {
+	if (text === null || text === undefined || text === "") {
+		return "";
+	} else {
+		text = text.replaceAll("&amp;#160;", " ");
+		text = text.replaceAll("&#160;", " ");
+		text = decodeHTMLEntities(text);
+		text = text.replaceAll("<br>", "<br />");
+		text = text.replaceAll("&", "&amp;");
+		return text;
+	}
+}
+
+function camelCase(data) {
+	var text = data
+		.replaceAll("2", "Two")
+		.replaceAll("3", "Three")
+		.replaceAll("4", "Four")
+		.replaceAll("5", "Five")
+		.replaceAll("w/ ", "")
+		.replaceAll("with ", "")
+		.replaceAll("the ", "")
+		.replaceAll("and ", "")
+		.replaceAll(/[^a-zA-Z0-9 .]+/g, "");
+	if (data.includes(" ")) {
+		var dataSplit = text.split(" ");
+		dataSplit.forEach(function (dS, i) {
+			if (i == 0) {
+				text = dS.toLowerCase();
+			} else {
+				text = text + dS;
+			}
+		});
+	} else {
+		text = text.toLowerCase();
+	}
+	return text;
+}
+
+function decodeHTMLEntities(text) {
+	var textArea = document.createElement("textarea");
+	textArea.innerHTML = text;
+	return textArea.value;
+}
+
+function encodeHTMLEntities(text) {
+	var textArea = document.createElement("textarea");
+	textArea.innerText = text;
+	return textArea.innerHTML;
+}
+
+function fieldValueTest(ct, n) {
+	listSubscribers({
+		type: "contenttype",
+		id: ct,
+	})
+		.then(function (result) {
+			console.log(result);
+			result.apiReturn.subscribers.forEach(function (sub) {
+				if (sub.type == "page") {
+					readAsset({
+						type: "page",
+						id: sub.id,
+					})
+						.then(function (result) {
+							console.log(result);
+							console.log(result.apiReturn.asset.page.path);
+							if (eval(n) != undefined) {
+								console.log("DEFINED: " + eval(n));
+							} else {
+								console.log("UNDEFINED");
+							}
+							console.log("-----------------");
+						})
+						.catch(function (error) {
+							console.log(error);
+							console.log("-----------------");
+						});
+				}
+			});
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
